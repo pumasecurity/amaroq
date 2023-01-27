b1dfd99611deb006b7fa0707c9bdee1ecea9cf5c
Eric Johnson <eric.johnson@pumasecurity.io>
2023-01-27 17:52:28 -0600

    feat: cosign digest + verify

--- build/publish-image.sh
+++ build/publish-image.sh
@@ -15,7 +15,8 @@ docker image tag ${AWS_PUBLIC_ECR_ORG_URI}/amaroq:${RELEASE_VERSION} ${AWS_PUBLI
 docker image push --all-tags ${AWS_PUBLIC_ECR_ORG_URI}/amaroq
 
 # sign image
-cosign sign ${AWS_PUBLIC_ECR_ORG_URI}/amaroq:${RELEASE_VERSION}
+IMAGE_DIGEST=$(docker image inspect ${AWS_PUBLIC_ECR_ORG_URI}/amaroq:${RELEASE_VERSION} | jq -r '.[].Id')
+cosign sign ${AWS_PUBLIC_ECR_ORG_URI}/amaroq@${IMAGE_DIGEST}
 
 # verify image
-cosign verify -output json ${ECR_REPOSITORY_URI}:${RELEASE_VERSION}
+cosign verify --output json ${AWS_PUBLIC_ECR_ORG_URI}/amaroq:${RELEASE_VERSION} | jq
